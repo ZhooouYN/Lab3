8f22a9756039f919265ae87d4452e0af2b1e5fa3
Zhou Yining <ZHOU.24@ichat.sp.edu.sg>
2025-05-12 17:29:07 +0800

    testing for bmi value

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,8 +1,25 @@
 import Lab2.bmi as bmi
 
 def test_bmi_normal_weight():
-   assert(bmi == 0)
+   bmi_value = 20
+   result = bmi.check_bmi(bmi_value)
+   if (bmi_value >= 18.5 and bmi_value <= 25.0):
+        print("you are very healthy")
+        return(0)
+   assert(result == 0)
+
 def test_bmi_over_weight():
-   assert(bmi == 1)
+   bmi_value = 30
+   result = bmi.check_bmi(bmi_value)
+   if (bmi_value > 25.0):
+        print("you are over weight")
+        return(1)
+   assert(result == 1)
+
 def test_bmi_under_weight():
-   assert(bmi == 1)
+   bmi_value = 11
+   result = bmi.check_bmi(bmi_value)
+   if (bmi_value < 18.5):
+        print("you are under weight")
+        return(-1)
+   assert(result == -1)
