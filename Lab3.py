1c4a97d8cdccc93fb6ee739a62ffac659e355a9a
Zhou Yining <ZHOU.24@ichat.sp.edu.sg>
2025-05-21 01:29:21 +0800

    latest version

--- Lab3.py
+++ Lab3.py
@@ -8,11 +8,15 @@ def bubble_sort(arr, sorting_order):
 
     # Copy input list to results list
     arr_result = arr.copy()
+    for item in arr_result:
+        if not isinstance(item, int):
+            arr_result =2
+            return arr_result 
+            
 
     # Get number of elements in the list
     n = len(arr_result)
-
-    if n < 10:
+    if 0 < n < 10:
         # Traverse through all array elements
         for i in range(n - 1):
             # range(n) also work but outer loop will
@@ -33,8 +37,10 @@ def bubble_sort(arr, sorting_order):
                 else:
                     # Return an empty array
                     arr_result = []
+    elif n > 10:
+        arr_result = 1
     else:
-        arr_result = -1
+        arr_result = 0
 
     return arr_result
 
